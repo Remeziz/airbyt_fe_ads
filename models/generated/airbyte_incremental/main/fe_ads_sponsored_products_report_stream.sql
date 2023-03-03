29c23d99fa3b66592c3144947dfcef4dea777442
Remeziz <79668248+Remeziz@users.noreply.github.com>
2023-03-03 11:58:58 +0200

    Add files via upload

---
+++ models/generated/airbyte_incremental/main/fe_ads_sponsored_products_report_stream.sql
@@ -0,0 +1,28 @@
+{{ config(
+    unique_key = '_airbyte_ab_id',
+    schema = "main",
+    tags = [ "top-level" ]
+) }}
+-- Final base SQL model
+-- depends_on: {{ ref('fe_ads_sponsored_products_report_stream_ab3') }}
+select
+    replace(json_extract(metric, '$."sku"') , '"', '') as sku,
+    replace(json_extract(metric, '$."asin"') , '"', '') as asin,
+    (replace(json_extract(metric, '$."cost"') , '"', '')) as cost,
+    (replace(json_extract(metric, '$."impressions"') , '"', '')) as impressions,
+    (replace(json_extract(metric, '$."clicks"') , '"', '')) as clicks,
+    (replace(json_extract(metric, '$."attributedSales1d"') , '"', '')) as sales,
+    (replace(json_extract(metric, '$."attributedUnitsOrdered1d"') , '"', '')) as orders,
+    profileid,
+    updatedat,
+    recordtype,
+    reportdate,
+    _airbyte_ab_id,
+    _airbyte_emitted_at,
+    {{ current_timestamp() }} as _airbyte_normalized_at,
+    _airbyte_fe_ads_spons___report_stream_hashid
+from {{ ref('fe_ads_sponsored_products_report_stream_ab3') }}
+-- fe_ads_sponsored_products_report_stream from {{ source('main', '_airbyte_raw_fe_ads_s__roducts_report_stream') }}
+where 1 = 1
+{{ incremental_clause('_airbyte_emitted_at', this) }}
+
